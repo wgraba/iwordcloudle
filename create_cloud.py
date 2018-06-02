6679a75dbc3df8c7c50de87d9b30b83289a30e61
Wesley Graba <wesley.graba@gmail.com>
2018-06-02 16:37:18 -0600

    * Added number of tweets to get as argument * Corrected removal of links and twitter handles

--- create_cloud.py
+++ create_cloud.py
@@ -15,6 +15,8 @@ if __name__ == "__main__":
     parser.add_argument('query', type=str, help='Term to query')
     parser.add_argument('output_path', type=str, help='Path to output file')
 
+    parser.add_argument('-c', '--count', default=5000, type=int, help='Number of tweets to get')
+
     args = parser.parse_args()
 
     # creating object of TwitterClient Class
@@ -22,16 +24,18 @@ if __name__ == "__main__":
                         args.access_token, args.access_token_secret)
 
     # calling function to get tweets
-    tweets = api.get_tweets(query=args.query, count=5000)
+    tweets = api.get_tweets(query=args.query, count=args.count)
 
+    print('Sample Tweets...')
     for tweet in tweets[:10]:
-        print(tweet)
+        print('Tweet: "{:s}"'.format(tweet))
 
     text = ' '.join(tweets)
 
     text = re.sub(args.query, '', text, flags=re.IGNORECASE)
-    text = re.sub('http.*[ \r\n]', '', text)
+    text = re.sub('http[s]?://[\.\w/]+', '', text)
     text = re.sub('RT', '', text)
+    text = re.sub('@\w+[:]?', '', text)
 
     wordcloud = WordCloud(max_font_size=40, margin=10, width=800, height=600, max_words=5000).generate(text)
     wordcloud.to_file(args.output_path)
