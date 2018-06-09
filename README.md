6755981a8fda4ac8f389cf4674b6edb07bc3c9fa
Wesley Graba <wesley.graba@gmail.com>
2018-06-09 16:19:22 -0600

    Add reading/writing from/to a file instead of querying Twitter every run

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # iwordcloudle
-Word cloud from Twitter.
+Word cloud from Twitter query.
 
 Uses the Twiter API to get the last n tweets with predetermined keywords from 
 Twitter, then -
 
 * Strip out common words like "the" and "a"
-* Create word cloud
+* Strip out words from Twitter query
+* Create word cloud with optional image as a stencil
 
 # Requirements
 * Python 3+
 * Tweepy
-* Textblob
 * Wordcloud
 
 # Usage
-Run `python3 create_cloud.py --help`
+Run `python3 create_cloud.py --help`.
 
 # Resources
 Uses [this tutorial](http://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/)
--- create_cloud.py
+++ create_cloud.py
@@ -14,38 +14,67 @@ if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('consumer_key')
-    parser.add_argument('consumer_secret')
-    parser.add_argument('access_token')
-    parser.add_argument('access_token_secret')
-    parser.add_argument('query', type=str, help='Terms to query separated by commas')
+    parser.add_argument('-k', '--consumer_key', help='Consumer key from Twitter app')
+    parser.add_argument('-s', '--consumer_secret', help='Consumer secret from Twitter app')
+    parser.add_argument('-t', '--access_token', help='Access token from Twitter app')
+    parser.add_argument('-a', '--access_token_secret', help='Secret access token from Twitter app')
+    parser.add_argument('-q', '--query', type=str, help='Terms to query separated by commas')
     parser.add_argument('output_path', type=str, help='Path to output file')
 
     parser.add_argument('-c', '--count', default=5000, type=int, help='Number of tweets to get')
+    parser.add_argument('-f', '--file', type=str, help='Input file of words instead of Twitter')
     parser.add_argument('-i', '--image', type=str, help='Path to image file to mask')
+    parser.add_argument('-w', '--write', type=str, help='Path to file to write Twitter output; can be used to cache'
+                                                        'Twitter results and used later with -f. -w is ignored if '
+                                                        '-f is used')
 
     args = parser.parse_args()
 
-    query_terms = args.query.split(',')
+    if args.file is not None:
+        with open(args.file, 'r') as in_file:
+            print('Reading from file instead of Twitter...')
+            text = in_file.read()
 
-    api = twitterClient.TwitterClient(args.consumer_key, args.consumer_secret,
-                        args.access_token, args.access_token_secret)
+    else:
+        # Check required argument for Twitter search
+        if args.query is None:
+            raise Exception('No query specified')
 
-    print('Getting tweets...')
-    tweets = api.get_tweets(query=' OR '.join(query_terms), count=args.count)
+        if args.consumer_key is None:
+            raise Exception('No consumer key specified')
 
-    print('\nFound {:d} Tweets; examples...'.format(len(tweets)))
-    for tweet in tweets[:10]:
-        print('Tweet: "{:s}"'.format(tweet))
+        if args.access_token is None:
+            raise Exception('No access token specified')
 
-    text = ' '.join(tweets)
+        if args.access_token_secret is None:
+            raise Exception('No access token secret specified')
 
-    print('\nCleaning tweets...')
-    text = re.sub('|'.join(query_terms), '', text, flags=re.IGNORECASE)
-    text = re.sub('http[s]?://[\.\w/]+', '', text)
-    text = re.sub('RT', '', text)
-    text = re.sub('@\w+[:]?', '', text)
-    text = re.sub('&amp', '', text)
+        # Perform twitter search
+        query_terms = args.query.split(',')
+
+        api = twitterClient.TwitterClient(args.consumer_key, args.consumer_secret,
+                                          args.access_token, args.access_token_secret)
+
+        print('Getting tweets...')
+        tweets = api.get_tweets(query=' OR '.join(query_terms), count=args.count)
+
+        print('\nFound {:d} Tweets; examples...'.format(len(tweets)))
+        for tweet in tweets[:10]:
+            print('Tweet: "{:s}"'.format(tweet))
+
+        text = ' '.join(tweets)
+
+        print('\nCleaning tweets...')
+        text = re.sub('|'.join(query_terms), '', text, flags=re.IGNORECASE)
+        text = re.sub('http[s]?://[\.\w/]+', '', text)
+        text = re.sub('RT', '', text)
+        text = re.sub('@\w+[:]?', '', text)
+        text = re.sub('&amp', '', text)
+
+        if args.write:
+            with open(args.write, 'w') as out_file:
+                print('Writing Twitter results to file...')
+                out_file.write(text)
 
     print('\nCreating word cloud...')
     if args.image is not None:
